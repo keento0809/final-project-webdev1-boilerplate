a22ee7000b1f44eb7f88ed287e6f1dd67ba8a083
KEENTO <handball0321@gmail.com>
2022-05-17 11:05:29 -0700

    Fix the fliter features

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -4,9 +4,14 @@ export const countries = getCountries();
 
 export let inputValue = "";
 
+let listing;
+let updateLoadedData;
+let options;
+
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
 
+  // fetch countries data
   const loadedData = [];
   const regions = [];
   const hash = {};
@@ -27,17 +32,59 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   }
 
+  options = regions.map((region) => {
+    return `
+      <option class='countries__selectOption' value=${region}>
+        ${region}
+      </option>
+    `;
+  });
+
   // Get DOM
   const body = document.querySelector("body");
   const nav = document.querySelector(".nav");
   const countriesSearchSec = document.querySelector(
     ".countries__searchSection"
   );
-  const countryCards = document.querySelectorAll(".countries__countryCard");
-
+  const countryList = document.querySelector(".countries__countryListSection");
   const toggleIcon = document.querySelector("#toggleIcon");
   const searchInput = document.querySelector(".inputAttribute");
   const regionSelect = document.querySelector(".regionSelect");
+
+  const selectTag = document.querySelector(".regionSelect");
+  selectTag.innerHTML = `
+  <option class='countries__selectOption' value='all'>Filter by region</option>
+  ${options}
+`;
+
+  function mapCountries(data) {
+    listing = data.map((country) => {
+      return `<li class=countries__countryCard>
+      <a href=/countries/${country.cca3} data-link>
+      <img src=${country.flagImg} alt="flag"/>
+      <div class="countries__countryCardDiv">
+      <div class="countries__countryName">
+      <h3>${country.name}</h3>
+      </div>
+      <p><strong>Population:</strong> <span class="countries__countryCardData">${country.population.toLocaleString()}</span></p>
+      <p><strong>Region:</strong> <span class="countries__countryCardData">${
+        country.region
+      }</span></p>
+      <p><strong>Capital:</strong> <span class="countries__countryCardData">${
+        country.capital
+      }</span></p>
+      </div>
+      </a>
+        </li>`;
+    });
+    if (listing.length === 0)
+      countryList.innerHTML = `<p>No result matched.</p>`;
+    else countryList.innerHTML = listing.join("");
+  }
+
+  mapCountries(loadedData);
+
+  const countryCards = document.querySelectorAll(".countries__countryCard");
 
   // original code
   searchInput.addEventListener("keyup", function (e) {
@@ -46,31 +93,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
     updateLoadedData = loadedData.filter((country) => {
       return country.name.toLowerCase().includes(inputValue);
     });
-    console.log(updateLoadedData);
-    // testFunc(updateLoadedData);
-    // return updateLoadedData;
+    mapCountries(updateLoadedData);
   });
 
   // original code
   regionSelect.addEventListener("change", function (e) {
     const selectedValue = e.target.value;
-
-    updateLoadedData = loadedData.filter((country) => {
-      return country.region === selectedValue;
-    });
-    console.log(updateLoadedData);
+    if (selectedValue === "all") updateLoadedData = loadedData;
+    else
+      updateLoadedData = loadedData.filter((country) => {
+        return country.region === selectedValue;
+      });
+    mapCountries(updateLoadedData);
   });
 
   function handleToggleMode() {
-    console.log("なぜに動かぬ");
-
     body.classList.toggle("darkMode");
     nav.classList.toggle("darkMode");
     countriesSearchSec.classList.toggle("darkMode");
-    countryCards.forEach((card) => card.classList.toggle("darkMode"));
+    countryList.classList.toggle("darkMode");
+    // countryCards.forEach((card) => card.classList.toggle("darkMode"));
   }
 
   toggleIcon.addEventListener("click", handleToggleMode);
 });
-
-export let updateLoadedData = "aa";
--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -1,5 +1,5 @@
 import AbstractView from "./AbstractView.js";
-import { countries, updateLoadedData } from "../index.js";
+import { countries } from "../index.js";
 
 export default class extends AbstractView {
   constructor(params) {
@@ -8,45 +8,7 @@ export default class extends AbstractView {
   }
 
   async getHTML() {
-    // test
-    const body = document.querySelector("body");
-    const nav = document.querySelector(".nav");
-
-    // window.location = function () {
-    //   console.log("ふひひひ日保ひおほいほほほいほい");
-    // };
-
-    const countriesSearchSec = document.querySelector(
-      ".countries__searchSection"
-    );
-    const countryCards = document.querySelectorAll(".countries__countryCard");
-    const toggleIcon = document.querySelector("#toggleIcon");
-    toggleIcon.addEventListener("click", function (e) {
-      body.classList.add("darkMode");
-      nav.classList.add("darkMode");
-      countriesSearchSec.classList.add("darkMode");
-      countryCards.forEach((card) => card.classList.add("darkMode"));
-    });
-
-    const loadedData = [];
     const regions = [];
-    const hash = {};
-
-    for (const key in countries) {
-      if (hash[countries[key].region] === undefined) {
-        hash[countries[key].region] = 1;
-        regions.push(countries[key].region);
-      }
-
-      loadedData.push({
-        cca3: countries[key].cca3,
-        name: countries[key].name.common,
-        population: countries[key].population,
-        region: countries[key].region,
-        capital: countries[key].capital,
-        flagImg: countries[key].flags.png,
-      });
-    }
 
     const options = regions.map((region) => {
       return `
@@ -56,38 +18,9 @@ export default class extends AbstractView {
       `;
     });
 
-    console.log(updateLoadedData);
-
     // display data(original)
-    let testLi = loadedData.map((country) => {
-      return `<li class=countries__countryCard>
-      <a href=/countries/${country.cca3} data-link>
-      <img src=${country.flagImg} alt="flag"/>
-      <div class="countries__countryCardDiv">
-      <div class="countries__countryName">
-      <h3>${country.name}</h3>
-      </div>
-      <p><strong>Population:</strong> <span class="countries__countryCardData">${country.population.toLocaleString()}</span></p>
-      <p><strong>Region:</strong> <span class="countries__countryCardData">${
-        country.region
-      }</span></p>
-      <p><strong>Capital:</strong> <span class="countries__countryCardData">${
-        country.capital
-      }</span></p>
-      </div>
-      </a>
-        </li>`;
-    });
-
-    // sorted by alphabetical order
-    testLi.sort((a, b) => {
-      return a < b ? -1 : 1;
-    });
-
     const countryList = document.createElement("ul");
     countryList.classList.add("countries__countryList");
-    countryList.innerHTML = `aaa`;
-    // console.log(countryList);
 
     // create searchInput
     const searchDiv = document.createElement("div");
@@ -102,15 +35,6 @@ export default class extends AbstractView {
     const selectTag = document.createElement("select");
     selectTagDiv.append(selectTag);
     selectTag.classList.add("regionSelect", "countries__searchDropdown");
-    selectTag.innerHTML = `
-              <option class='countries__selectOption'>Filter by region</option>
-              ${options}
-            `;
-
-    window.addEventListener("DOMContentLoaded", function (e) {
-      const toggleIcon = document.querySelector("#toggleIcon");
-      console.log(toggleIcon);
-    });
 
     return `<div class="container">
     <div class="wrapper">
@@ -119,7 +43,6 @@ export default class extends AbstractView {
     ${selectTagDiv.innerHTML}
     </section>
     <section class="countries__countryListSection">
-      ${testLi.join("")}
     </section>
     </div>
     </div>`;
